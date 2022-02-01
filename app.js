52dcf59fbe5defaa71fd4b30247b4056a8184cfd
Kis Zoltán <kis.zoltan.cs@gmail.com>
2022-02-01 11:20:32 +0100

    Added express js middleware and routing

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ const path = require('path');
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
+const rootDir = require('./utils/path');
 
 
 //Server application
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/admin',adminRouter);
 app.use(shopRouter);
+app.use('/admin',adminRouter);
 app.use((req,res,next) => 
 {
-	res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'));
+	res.status(404).sendFile(path.join(rootDir,'views','page-not-found.html'));
 });
 
 app.listen(3000);

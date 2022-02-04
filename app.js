c153fbbd1b9937cd36a5731f22060c85556d5333
Kis Zoltán <kis.zoltan.cs@gmail.com>
2022-02-04 11:25:29 +0100

    Added html template engines

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-//Modules
-const http = require('http');
-const express = require('express'); 
-const bodyParser =  require('body-parser');
-const app = express();
 const path = require('path');
 
-const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const rootDir = require('./utils/path');
+const app = express();
 
+/*To use handlebar. For some reason it not works, but I don't like it either, so I dont bother, to find why! :P*/
+//const handlebarEngine = require('express-handlebars');
+//app.engine('handlebars',handlebarEngine());
+//app.set('view engine', 'handlebars')
 
-//Server application
-app.use(bodyParser.urlencoded({extended: false}));
+/*To use ejs. I think thats the best to us*/
+app.set('view engine', 'ejs');
 
-app.use(shopRouter);
-app.use('/admin',adminRouter);
-app.use((req,res,next) => 
-{
-	res.status(404).sendFile(path.join(rootDir,'views','page-not-found.html'));
+/*To use pug. Not bad, but I like the ejs more*/
+//app.set('view engine', 'pug');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
-console.log('Server is running');

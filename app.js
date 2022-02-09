b39b7c5a98f656767cfc9bb23ed6ca50d1afe032
Kis Zoltán <kis.zoltan.cs@gmail.com>
2022-02-09 11:18:27 +0100

    Added controllers

--- app.js
+++ app.js
@@ -16,17 +16,18 @@ app.set('view engine', 'ejs');
 /*To use pug. Not bad, but I like the ejs more*/
 //app.set('view engine', 'pug');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
+// app.use((req, res, next) => {
+//   res.status(404).render('404', { pageTitle: 'Page Not Found' });
+// });
 
 app.listen(3000);

76918af288eec46bca4d30f7869ed9454c80f64b
Hir0ta <kis.zoltan.cs@gmail.com>
2022-02-15 11:03:45 +0100

    refactoring and new pages

--- app.js
+++ app.js
@@ -3,31 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
-/*To use handlebar. For some reason it not works, but I don't like it either, so I dont bother, to find why! :P*/
-//const handlebarEngine = require('express-handlebars');
-//app.engine('handlebars',handlebarEngine());
-//app.set('view engine', 'handlebars')
-
-/*To use ejs. I think thats the best to us*/
 app.set('view engine', 'ejs');
-
-/*To use pug. Not bad, but I like the ejs more*/
-//app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use(errorController.get404);
-// app.use((req, res, next) => {
-//   res.status(404).render('404', { pageTitle: 'Page Not Found' });
-// });
 
 app.listen(3000);
